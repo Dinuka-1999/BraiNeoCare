e8ec92b9be0b8a6744e2c78627dd1a7066758315
nima34366 <wickramasinghenlssck@gmail.com>
2023-09-01 22:54:20 +0530

    after dry electrode 1st test

--- wifi_data_incoming.py
+++ wifi_data_incoming.py
@@ -13,7 +13,7 @@ from tkinter import *
 
 
 def update_menu():
-    global lp_range_var, lp_order_var, hp_range_var, hp_order_var, bp_range_low_var, bp_range_high_var, bp_order_var, bs_range_low_var, bs_range_high_var, bs_order_var
+    global lp_range_var, lp_order_var, hp_range_var, hp_order_var, bp_range_low_var, bp_range_high_var, bp_order_var, bs_range_low_var, bs_range_high_var, bs_order_var, ap_range_low_var, ap_range_high_var, ap_order_var, mp_kernel_var, bs2_range_low_var, bs2_range_high_var, bs2_order_var, mpb_kernel_var
     while True:
         lp_range_var = float(lp_range.get())
         lp_order_var = int(lp_order.get())
@@ -24,12 +24,22 @@ def update_menu():
         bp_order_var = int(bp_order.get())
         bs_range_low_var = float(bs_range_low.get())
         bs_range_high_var = float(bs_range_high.get())
+        bs2_order_var = int(bs2_order.get())
+        bs2_range_low_var = float(bs2_range_low.get())
+        bs2_range_high_var = float(bs2_range_high.get())
         bs_order_var = int(bs_order.get())
+        ap_range_low_var = float(ap_range_low.get())
+        ap_range_high_var = float(ap_range_high.get())
+        ap_order_var = int(ap_order.get())
+        mp_kernel_var = int(mp_kernel.get())
+        mpb_kernel_var = int(mpb_kernel.get())
+
+
 
         time.sleep(0.001)
 
 def menu():
-    global lp_check_var, lp_range, lp_order, hp_check_var, hp_range, hp_order, bp_check_var, bp_range_low, bp_range_high, bp_order, bs_check_var, bs_range_low, bs_range_high, bs_order
+    global lp_check_var, lp_range, lp_order, hp_check_var, hp_range, hp_order, bp_check_var, bp_range_low, bp_range_high, bp_order, bs_check_var, bs_range_low, bs_range_high, bs_order, bs2_check_var, bs2_range_low, bs2_range_high, bs2_order, ap_check_var, ap_range_low, ap_range_high, ap_order, mp_check_var, mp_kernel, mpb_check_var, mpb_kernel
     master = Tk()
     master.title("Menu")
     lp_frame = Frame(master)
@@ -40,6 +50,14 @@ def menu():
     bp_frame.pack(side=TOP)
     bs_frame = Frame(master)
     bs_frame.pack(side=TOP)
+    bs2_frame = Frame(master)
+    bs2_frame.pack(side=TOP)
+    ap_frame = Frame(master)
+    ap_frame.pack(side=TOP)
+    mp_frame = Frame(master)
+    mp_frame.pack(side=TOP)
+    mpb_frame = Frame(master)
+    mpb_frame.pack(side=TOP)
 
     lp_check_var = IntVar()
     Checkbutton(lp_frame, text="Lowpass", variable=lp_check_var).pack(side=LEFT)
@@ -91,9 +109,52 @@ def menu():
     bs_order.insert(0, "2")
     bs_order.pack(side=LEFT)
 
+    bs2_check_var = IntVar()
+    Checkbutton(bs2_frame, text="Bandstop", variable=bs2_check_var).pack(side=LEFT)
+    Label(bs2_frame, text="Range").pack(side=LEFT)
+    bs2_range_low = Entry(bs2_frame)
+    bs2_range_low.insert(0, "95")
+    bs2_range_low.pack(side=LEFT)
+    bs2_range_high = Entry(bs2_frame)
+    bs2_range_high.insert(0, "105")
+    bs2_range_high.pack(side=LEFT)
+    Label(bs2_frame, text="Order").pack(side=LEFT)
+    bs2_order = Entry(bs2_frame)
+    bs2_order.insert(0, "2")
+    bs2_order.pack(side=LEFT)
+
+    ap_check_var = IntVar()
+    Checkbutton(ap_frame, text="Alpha", variable=ap_check_var).pack(side=LEFT)
+    Label(ap_frame, text="Range").pack(side=LEFT)
+    ap_range_low = Entry(ap_frame)
+    ap_range_low.insert(0, "8")
+    ap_range_low.pack(side=LEFT)
+    ap_range_high = Entry(ap_frame)
+    ap_range_high.insert(0, "13")
+    ap_range_high.pack(side=LEFT)
+    Label(ap_frame, text="Order").pack(side=LEFT)
+    ap_order = Entry(ap_frame)
+    ap_order.insert(0, "2")
+    ap_order.pack(side=LEFT)
+
+    mp_check_var = IntVar()
+    Checkbutton(mp_frame, text="Median", variable=mp_check_var).pack(side=LEFT)
+    Label(mp_frame, text="Kernel").pack(side=LEFT)
+    mp_kernel = Entry(mp_frame)
+    mp_kernel.insert(0, "3")
+    mp_kernel.pack(side=LEFT)
+
+    mpb_check_var = IntVar()
+    Checkbutton(mpb_frame, text="Median Baseline", variable=mpb_check_var).pack(side=LEFT)
+    Label(mpb_frame, text="Kernel").pack(side=LEFT)
+    mpb_kernel = Entry(mpb_frame)
+    mpb_kernel.insert(0, "151")
+    mpb_kernel.pack(side=LEFT)
+
+
     mainloop()
 
-UDP_IP = "192.168.177.143" # The IP that is printed in the serial monitor from the ESP32
+UDP_IP = "192.168.235.143" # The IP that is printed in the serial monitor from the ESP32
 SHARED_UDP_PORT = 4210
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet  # UDP
 sock.connect((UDP_IP, SHARED_UDP_PORT))
@@ -108,7 +169,7 @@ baseLine_plot=[]
 alpha_plot=[]
 fft_plot=[]
 
-for j,i in enumerate(show):
+for i in show:
     plot.append(win.addPlot(title=f'Channel {i}'))
     orig_plot.append(win.addPlot(title=f'Original {i}'))
     alpha_plot.append(win.addPlot(title=f'Decision {i}'))
@@ -153,7 +214,7 @@ def update_plot():
 
 
 def filter():
-    global rate, filtered, ydata_avg, alpha, yfft, xfft
+    global rate, filtered, ydata_avg, alpha, yfft, xfft, mpb_kernel_var
     T = 1/rate
     xfft = fftfreq(1000, T)[:1000//2]
     while True:
@@ -161,7 +222,9 @@ def filter():
         lowpass = signal.butter(lp_order_var, lp_range_var, 'lp', fs=rate, output='sos')
         highpass = signal.butter(hp_order_var, hp_range_var, 'hp', fs=rate, output='sos')
         bandstop = signal.butter(bs_order_var, [bs_range_low_var,bs_range_high_var], 'bandstop', fs=rate, output='sos')
+        bandstop2 = signal.butter(bs2_order_var, [bs2_range_low_var,bs2_range_high_var], 'bandstop', fs=rate, output='sos')
         bandpass = signal.butter(bp_order_var, [bp_range_low_var,bp_range_high_var], 'bandpass', fs=rate, output='sos')
+        alphapass = signal.butter(ap_order_var, [ap_range_low_var,ap_range_high_var], 'bandpass', fs=rate, output='sos')
                 
         ydata_avg = ydata - np.mean(ydata,axis=1).reshape(-1,1)
         filtered_ = ydata_avg
@@ -174,14 +237,25 @@ def filter():
             filtered_ = signal.sosfilt(bandpass, filtered_)
         if bs_check_var.get() == 1:
             filtered_ = signal.sosfilt(bandstop, filtered_)
+        if bs2_check_var.get() == 1:
+            filtered_ = signal.sosfilt(bandstop2, filtered_)
+
+        if mpb_check_var.get() == 1:
+            baseline=np.zeros((len(show),ydata.shape[1]))
+            for i in range(len(show)):
+                if mpb_kernel_var%2==0:
+                    mpb_kernel_var+=1
+                baseline[i]=signal.medfilt(filtered_[i], kernel_size=mpb_kernel_var)
+                filtered_ = filtered_ - baseline
 
         # filtered=np.zeros((len(show),ydata.shape[1]))
-        # for i in range(len(show)):
-        #     filtered[i]=signal.medfilt(filtered3[i], kernel_size=9)
+        if mp_check_var.get() == 1:
+            for i in range(len(show)):
+                filtered_[i]=signal.medfilt(filtered_[i], kernel_size=mp_kernel_var)
 
         filtered = filtered_
         
-        alpha = signal.sosfilt(bandpass, filtered)
+        alpha = signal.sosfilt(alphapass, filtered)
 
         yfft = np.abs(fft(filtered))
             
@@ -196,18 +270,18 @@ if __name__ == "__main__":
     sock.send('Hello ESP32'.encode())
     t7 = threading.Thread(target=menu)
     t7.start()
-    time.sleep(0.01)
+    time.sleep(0.1)
     t8 = threading.Thread(target=update_menu)
     t8.start()
-    time.sleep(0.01)
+    time.sleep(0.1)
     t1 = threading.Thread(target=loop)
     t1.start()
-    time.sleep(0.01)
+    time.sleep(0.1)
     t3 = threading.Thread(target=filter)
     t3.start()
-    time.sleep(0.01)
+    time.sleep(0.1)
     t6 = threading.Thread(target=update_plot)
     t6.start()
-    time.sleep(0.01)
+    time.sleep(0.1)
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QApplication.instance().exec_()

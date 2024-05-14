f11c831308edead9d0616d505d9886e7d3f09e24
Dinuka-1999 <81279517+Dinuka-1999@users.noreply.github.com>
2024-05-14 19:57:37 +0530

    Minor changes

--- XAI.py
+++ XAI.py
@@ -14,8 +14,6 @@ EEG,labels=rs.read_a_file("...Path to your EEG data file(.edf)...","...File numb
 model = keras.models.load_model("...Path to your model...")
 model.layers[-1].activation = None
 
-model.summary()
-
 grad_model = keras.Model(model.inputs,[model.get_layer("...Name of the final GAT layer...").output,model.output])
 
 def PreprocesSignal(signal,mean,std):
@@ -39,48 +37,78 @@ def GradCAM(signal,model=grad_model):
 
 mean,std=np.load("...Path to mean and std file...")#mean and std of the training data
 
-fig,ax=plt.subplots(12,1,figsize=(20,20))
 
-lb= 64600#Lower bound of the required time frame
-ub= 64600+1152#Upper bound of the required time frame (1152 samples = 32 seconds)
-for r in range(12):
-    ax[r].plot(EEG[r][lb:ub])
-    ax[r].plot(labels[r][lb:ub])
-    # ax[r].set_title(channel_names[r])
-fig.tight_layout()
-plt.show()
-
-channel_names=["Fp1-T3","T3-O1","Fp1-C3","C3-O1","Fp2-C4","C4-O2","Fp2-T4","T4-O2","T3-C3","C3-Cz","Cz-C4","C4-T4"]
+channel_names=["Fp1 - T3","T3 - O1","Fp1 - C3","C3 - O1","Fp2 - C4","C4 - O2","Fp2 - T4","T4 - O2","T3 - C3","C3 - Cz","Cz - C4","C4 - T4"]
 cmap = plt.cm.bwr
 
-l=8100
-u=8100+1152
-h_map=np.zeros((12,1152))
-for r in range(3):
-    lb=8100+384*r
-    ub=8100+384*r+384
+l=0
+u=10*60*32-4800+192
+True_labels=np.zeros((1,u-l))
+True_labels[0,266*32-l:971*32-l]=1
+Pred_labels=np.zeros((1,u-l))
+h_map=np.zeros((12,u-l))
+previous=0
+for r in range(u//384):
+    lb=l+r*384
+    ub=l+r*384+384
     x=PreprocesSignal(EEG[:,lb:ub],mean,std)
     h,p=GradCAM(x)
-    print("prediction:- ",1 if p>=0.5 else 0)
-    if p>=0.5:
+    Pred_labels[0,r*384:(r+1)*384]=p
+    if p>=0.61:
         resized_heatmap = cv.resize(h, (384,12), interpolation=cv.INTER_LINEAR)
-        h_map[:,384*r:384*r+384]=resized_heatmap
-        
-# plt.style.use('dark_background')
-fig,ax=plt.subplots(12,1,figsize=(20,25))
+        if previous==0:
+            h_map[:,384*r:384*r+384]=resized_heatmap
+            previous=1
+        else:
+            h_map[:,384*r:384*r+384]=resized_heatmap
+            for i in range(12):
+                if (h_map[i,384*r]<0.15 and h_map[i,384*r-1]>0.85):
+                    for j in range(15):
+                        h_map[i,384*r-j]=0.5+j*0.023
+                        h_map[i,384*r+1+j]=0.5-j*0.023
+            previous=1
+    else:
+        previous=0
+
+
+# plt.style.use('yellow_background')
+fig,ax=plt.subplots(14,1,figsize=(20,40))
+
+time= np.arange(0,7.6,1/(32*60))
+ax[0].plot(time,True_labels[0],label="True labels",color='purple')
+ax[0].plot(time,Pred_labels[0],label="Predicted labels",color='green')
+ax[0].set_xlim([0,7.5])
+ax[0].set_title("Seizure Probability",fontsize=27)
+ax[0].hlines(0.61,0,7.5,linestyles='dashed',color='black',label="Threshold")
+ax[0].tick_params(axis='x', labelsize=20)
+ax[0].tick_params(axis='y', labelsize=20)
+ax[0].legend(fontsize=15)
+
 norm = plt.Normalize(vmin=0, vmax=1)
-b=np.arange(0,36,1/32)
-for r in range(12):
-    for i in range(1151):
-        ax[r].plot([i/32,(i+1)/32],[EEG[r,l+i]*1e6,EEG[r,l+i+1]*1e6],color=cmap(norm(h_map[r,i])))
-    ax[r].set_title(channel_names[r],fontsize=20)   
-    ax[r].set_xlim([0,36])
-    ax[r].tick_params(axis='x', labelsize=16)
-    ax[r].tick_params(axis='y', labelsize=16)
-ax[-1].set_xlabel("Time (s)",fontsize=20)
+b=np.arange(0,7.5,1/(32*60))
+for r in range(1,13):
+    for i in range(u-l):
+        ax[r].plot([i/(32*60),(i+1)/(32*60)],[EEG[r-1,l+i]*1e6,EEG[r-1,l+i+1]*1e6],color=cmap(norm(h_map[r-1,i])))
+    ax[r].set_title(channel_names[r-1],fontsize=27)   
+    ax[r].set_xlim([0,7.5])
+    # ax[r].set_ylim(-28,28)
+    ax[r].tick_params(axis='x', labelsize=20)
+    ax[r].tick_params(axis='y', labelsize=20)
+ax[-1].set_xlabel("Time (min)",fontsize=30)
+fig.text(-0.015, 0.6, 'Amplitude (uV)', va='center', rotation='vertical', fontsize=30)
+
+for i in range(1152+384):
+        ax[13].plot([i/(32*60),(i+1)/(32*60)],[EEG[0,8116-384+i]*1e6,EEG[0,8116-384+i+1]*1e6],color=cmap(norm(h_map[0,3316-384+4800+i])))
+ax[13].set_xticks(np.linspace(0,48/60,4),np.linspace(4.02, 4.83, num=4)) 
+ax[13].tick_params(axis='x', labelsize=20)
+ax[13].tick_params(axis='y', labelsize=20)
+ax[13].set_title("Zoomed in time window of Fp1 - T3",fontsize=27)
 fig.tight_layout()
-fig.text(-0.015, 0.6, 'Amplitude (uV)', va='center', rotation='vertical', fontsize=20)
-im=ax[0].imshow(h_map[0].reshape(1,1152),cmap=cmap,alpha=1,extent=[0,36,EEG[0,l:u].min()*1e6,EEG[0,l:u].max()*1e6],aspect='auto',vmax=1,vmin=0,visible=False)
+
+plt.subplots_adjust(hspace=0.5)
+im=ax[1].imshow(h_map[0].reshape(1,u-l),cmap=cmap,alpha=1,extent=[0,7.5,EEG[0,l:u].min()*1e6,EEG[0,l:u].max()*1e6],aspect='auto',vmax=1,vmin=0,visible=False)
 cbar=fig.colorbar(im,ax=ax,orientation='horizontal',pad=0.04,shrink=0.5)
-cbar.set_label('Relevance',fontsize=20)
+cbar.ax.tick_params(labelsize=22)
+cbar.set_label('Relevance',fontsize=30)
+plt.savefig("EX_AI.png",bbox_inches='tight',dpi=200)
 plt.show()
